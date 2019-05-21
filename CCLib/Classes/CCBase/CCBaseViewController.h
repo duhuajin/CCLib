027902c8660a10d9e9011958f887b223b6656abf
Du <552923375@qq.com>
2019-05-21 11:54:01 +0800

    base added realHeight

--- CCLib/Classes/CCBase/CCBaseViewController.h
+++ CCLib/Classes/CCBase/CCBaseViewController.h
@@ -11,20 +11,16 @@
 
 @interface CCBaseViewController : UIViewController
 
-+ (instancetype)appear ; //初始化一个通用的baseviewcontroller 用于设置样式,如返回按钮
 @property (strong,nonatomic)UIImage *backImage;
+@property (copy,nonatomic)void(^needRefreshBlock)(void);
+@property (nonatomic)BOOL hideNavi;
+@property (nonatomic)float realHeight;
 
++ (instancetype)appear ; //初始化一个通用的baseviewcontroller 用于设置样式,如返回按钮
 -(void)pop:(NSInteger)index;
 -(void)pop;
-
-@property (nonatomic)BOOL hideNavi;
-
-@property (copy,nonatomic)void(^needRefreshBlock)(void);
-
-
 -(void)showLoadingView;
 -(void)hideLoadingView;
-
 -(void)initBase;
 
 @end
